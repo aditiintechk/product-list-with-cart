4063177d1df5e5b42eab285683c87f0b02f72417
Aditi <aditideshpande2908@gmail.com>
2024-08-05 14:25:01 +0530

    style add to cart button

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
+import cartImg from '../assets/icons/icon-add-to-cart.svg'
+import plus from '../assets/icons/icon-increment-quantity.svg'
+import minus from '../assets/icons/icon-decrement-quantity.svg'
+
 export default function Items(props) {
 	const { image, name, category, price } = props
+	const [count, setCount] = useState(0)
 	return (
 		<section className='item-section'>
 			<img className='item-img' src={image} alt='' />
-			{/* <div className='add-to-cart'>
-				<button className='add-btn'>add to cart</button>
-			</div> */}
+			<div className='addtocart-container'>
+				{count === 0 ? (
+					<button className='addtocart-btn'>
+						<img src={cartImg} />
+						Add to Cart
+					</button>
+				) : (
+					<div className='addtocart-section'>
+						<img className='sign' src={minus} alt='minus symbol' />
+						<span className='count'>{count}</span>
+						<img className='sign' src={plus} alt='plus symbol' />
+					</div>
+				)}
+			</div>
 			<h5 className='item-category'>{category}</h5>
 			<h4 className='item-name'>{name}</h4>
 			<h4 className='item-price'>${price}</h4>
